64616959a833096920bfd1486eb104389e1407a1
Tanapat <iamtnont@gmail.com>
2026-03-22 23:06:17 +0700

    try to adjust tile layers thing

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -2,13 +2,22 @@ extends StaticBody2D
 
 @export var room_id: int = 0
 
-@onready var sprite = $AnimatedSprite2D
+# Using a list or a container makes it easier to manage multiple parts
+@onready var sprites = [$AnimatedSprite2D, $AnimatedSprite2D2]
 @onready var collision = $CollisionShape2D
 
 func _ready():
-	sprite.play("Closed")
+	_play_all("Closed")
+	# Force the top part to stay above the bottom part to stop blinking
+	$AnimatedSprite2D.z_index = 1 
+	$AnimatedSprite2D2.z_index = 0
 
 func open():
-	sprite.play("Open")
+	_play_all("Open")
 	collision.set_deferred("disabled", true)
 	print("Door for room ", room_id, " is open")
+
+# Helper function so you don't have to write the same line twice
+func _play_all(anim_name: String):
+	for sprite in sprites:
+		sprite.play(anim_name)
