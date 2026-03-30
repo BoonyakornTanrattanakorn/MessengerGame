948466693da6d6c3369076c2579a01145473ef02
besttny <6630120321@student.chula.ac.th>
2026-03-30 23:06:34 +0700

    feat: redesign chap1node1 again and add black edge to chap1node3, finaldoor bug fix, warp in node 3 bug fix

--- scripts/warp_to_level_1.gd
+++ scripts/warp_to_level_1.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var next_level_path: String = "res://assets/maps/levels/Level_1.scn"
-@export var spawn_position_in_next_level: Vector2 = Vector2(-846, 348)
+@export var spawn_position_in_next_level: Vector2 = Vector2(-956, 348)
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
