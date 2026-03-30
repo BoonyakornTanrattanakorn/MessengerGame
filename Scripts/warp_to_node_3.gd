0b69564a7d95c03ff287a305c613c3d46cd5b66b
Theerach2005 <theerat.sc@gmail.com>
2026-03-30 18:43:11 +0700

    chapter1 node3_0 and node3_2 map redesign

--- Scripts/warp_to_node_3.gd
+++ Scripts/warp_to_node_3.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var next_level_path: String = "res://assets/maps/levels/Level_0.scn"
-@export var spawn_position_in_next_level: Vector2 = Vector2(29, 317)
+@export var spawn_position_in_next_level: Vector2 = Vector2(-750, 450)
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
