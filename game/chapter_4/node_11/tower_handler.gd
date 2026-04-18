360ce4f4777ce91e7ce1d7758cdb48dc617a9e3e
jednipit-golf <jednipit_golf@hotmail.com>
2026-04-19 01:12:45 +0700

    add dialogue

--- game/chapter_4/node_11/tower_handler.gd
+++ game/chapter_4/node_11/tower_handler.gd
@@ -1,5 +1,7 @@
 extends LevelEventHandler
 
+const ICE_GHOST_DIALOGUE := preload("res://game/chapter_4/node_11/dialogue/ice_ghost.dialogue")
+
 @export var tower: Node2D
 @export var focus_delay: float = 0.2
 @export var focus_duration: float = 1.5
@@ -18,5 +20,8 @@ func handle_intro_for_level() -> void:
 
 	await get_tree().create_timer(focus_delay).timeout
 	player.focus_camera_to(tower)
+	if ICE_GHOST_DIALOGUE != null:
+		DialogueManager.show_dialogue_balloon(ICE_GHOST_DIALOGUE, "start", [self])
+		await DialogueManager.dialogue_ended
 	await get_tree().create_timer(focus_duration).timeout
 	player.return_camera()
