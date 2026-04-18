1145290bcfe282435951f27d41bc63208016a236
jednipit-golf <jednipit_golf@hotmail.com>
2026-04-18 22:33:53 +0700

    add gamestate fix name

--- game/chapter_4/node_11/node_11_event_handler.gd
+++ game/chapter_4/node_11/node_11_event_handler.gd
@@ -7,13 +7,16 @@ func on_level_loaded() -> void:
 	pass
 
 func handle_intro_for_level() -> void:
-	if tower != null:
-		player.focus_camera_to(tower)
-		await get_tree().create_timer(1.5).timeout
-		player.return_camera()
-	await get_tree().create_timer(1.0).timeout
+	if not GameState.chap4_node11_shown:
+		GameState.chap4_node11_shown = true
 
-	if castle != null:
-		player.focus_camera_to(castle)
-		await get_tree().create_timer(1.5).timeout
-		player.return_camera()
+		if tower != null:
+			player.focus_camera_to(tower)
+			await get_tree().create_timer(1.5).timeout
+			player.return_camera()
+			await get_tree().create_timer(1.0).timeout
+
+		if castle != null:
+			player.focus_camera_to(castle)
+			await get_tree().create_timer(1.5).timeout
+			player.return_camera()
