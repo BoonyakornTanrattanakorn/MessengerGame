ccef24a50ee02723ea6113b888a76a73c6fa1eaa
jednipit-golf <jednipit_golf@hotmail.com>
2026-04-18 22:21:33 +0700

    add focus camera

--- game/chapter_4/node_11/node_11_event_handler.gd
+++ game/chapter_4/node_11/node_11_event_handler.gd
@@ -1,9 +1,19 @@
 extends LevelEventHandler
+@export var tower: Node2D
+@export var castle: Node2D
 
 func on_level_loaded() -> void:
 	# Reserved for node-specific initialization after the level is loaded.
 	pass
 
 func handle_intro_for_level() -> void:
-	# Node11 currently has no intro cutscene.
-	pass
+	if tower != null:
+		player.focus_camera_to(tower)
+		await get_tree().create_timer(1.5).timeout
+		player.return_camera()
+	await get_tree().create_timer(1.0).timeout
+
+	if castle != null:
+		player.focus_camera_to(castle)
+		await get_tree().create_timer(1.5).timeout
+		player.return_camera()
