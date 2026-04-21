8cbb76b01dd1f016f5d861fd6a610f7a28b3a4b6
jednipit-golf <jednipit_golf@hotmail.com>
2026-04-21 16:50:23 +0700

    fix warp position

--- game/chapter_4/node_11/warp_to_floor_2.gd
+++ game/chapter_4/node_11/warp_to_floor_2.gd
@@ -3,7 +3,7 @@ extends Warp
 func _ready() -> void:
 	super._ready()
 	next_level_path = "res://game/chapter_4/node_11/tower_2nd_flr.tscn"
-	spawn_position_in_next_level = Vector2(260, 130)
+	spawn_position_in_next_level = Vector2(286,159)
 	facing_direction_on_warp = Vector2.DOWN
 
 func _on_body_entered(body: Node) -> void:
